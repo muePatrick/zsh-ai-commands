f46dbfdb9af33409c23039ede87a9e6c415a5581
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-05-21 16:56:10 +0200

    feat: add an explainer mode

--- zsh-ai-commands.zsh
+++ zsh-ai-commands.zsh
@@ -11,6 +11,8 @@
 
 (( ! ${+ZSH_AI_COMMANDS_N_GENERATIONS} )) && typeset -g ZSH_AI_COMMANDS_N_GENERATIONS=10
 
+(( ! ${+ZSH_AI_COMMANDS_EXPLAINER} )) && typeset -g ZSH_AI_COMMANDS_EXPLAINER=true
+
 fzf_ai_commands() {
   setopt extendedglob
 
@@ -23,10 +25,36 @@ fzf_ai_commands() {
   zle end-of-line
   zle reset-prompt
 
-  ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command."
-
+  if [ $ZSH_AI_COMMANDS_EXPLAINER = true ]
+  then
+    ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command. If your command is complicated and uses arguments, you MUST end your shell command with a shell comment starting with ## with a very concise explanation but your whole answer must remain a oneliner. Consider each new question from the user as independant."
   ZSH_AI_COMMANDS_GPT_USER="Description of what the command should do:\n'''\n$ZSH_AI_COMMANDS_USER_QUERY\n'''\nGive me the appropriate command."
-
+    ZSH_AI_COMMANDS_GPT_REQUEST_BODY='{
+    "model": "'$ZSH_AI_COMMANDS_LLM_NAME'",
+    "n": '$ZSH_AI_COMMANDS_N_GENERATIONS',
+    "temperature": 1,
+    "messages": [
+      {
+        "role": "system",
+        "content": "'$ZSH_AI_COMMANDS_GPT_SYSTEM'"
+      },
+      {
+        "role": "user",
+        "content": "Description of what the command should do:\n'''"\nlist files, sort by descending size\n"'''\nGive me the appropriate command."
+      },
+      {
+        "role": "assistant",
+        "content": "ls -lS ## -l long listing -S sort by file size -r reverse order"
+      },
+      {
+        "role": "user",
+        "content": "'$ZSH_AI_COMMANDS_GPT_USER'"
+      }
+    ]
+  }'
+  else
+    ZSH_AI_COMMANDS_GPT_SYSTEM="You only answer 1 appropriate shell one liner that does what the user asks for. The command has to work with the $(basename $SHELL) terminal. Don't wrap your answer in anything, dont acknowledge those rules, don't format your answer. Just reply the plaintext command."
+  ZSH_AI_COMMANDS_GPT_USER="Description of what the command should do:\n'''\n$ZSH_AI_COMMANDS_USER_QUERY\n'''\nGive me the appropriate command."
   ZSH_AI_COMMANDS_GPT_REQUEST_BODY='{
     "model": "'$ZSH_AI_COMMANDS_LLM_NAME'",
     "n": '$ZSH_AI_COMMANDS_N_GENERATIONS',
@@ -43,13 +71,24 @@ fzf_ai_commands() {
     ]
   }'
 
+
+  fi
+
+
+
   ZSH_AI_COMMANDS_GPT_RESPONSE=$(curl -q --silent https://api.openai.com/v1/chat/completions \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $ZSH_AI_COMMANDS_OPENAI_API_KEY" \
     -d "$ZSH_AI_COMMANDS_GPT_REQUEST_BODY")
   local ret=$?
 
+  if [ $ZSH_AI_COMMANDS_EXPLAINER = true ]
+  then
+      ZSH_AI_COMMANDS_SUGGESTIONS=$(echo $ZSH_AI_COMMANDS_GPT_RESPONSE | jq -r '.choices[].message.content' | uniq | sort | awk -F ' *## ' '!seen[$1]++' -)
+
+else
   ZSH_AI_COMMANDS_SUGGESTIONS=$(echo $ZSH_AI_COMMANDS_GPT_RESPONSE | jq -r '.choices[].message.content' | uniq)
+fi
 
   ZSH_AI_COMMANDS_SELECTED=$(echo $ZSH_AI_COMMANDS_SUGGESTIONS | fzf)
 
