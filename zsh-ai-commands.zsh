37441ede30b0a5ec485d9dd97ba4400da7cbc720
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-06-11 12:37:30 +0200

    minor

--- zsh-ai-commands.zsh
+++ zsh-ai-commands.zsh
@@ -18,7 +18,7 @@ fzf_ai_commands() {
 
   [ -n "$BUFFER" ] || { echo "Empty prompt" ; return }
 
-  BUFFER=$(echo "$BUFFER" | sed 's/^AI_ASK: //g')
+  BUFFER="$(echo "$BUFFER" | sed 's/^AI_ASK: //g')"
 
   ZSH_AI_COMMANDS_USER_QUERY=$BUFFER
 
